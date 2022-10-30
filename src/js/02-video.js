18a9aaf4b2e2fef8c7436823f68b2d2008ac7a72
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-10-30 20:56:08 +0200

    do all homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const TIME = "videoplayer-current-time"
+const iframe = document.querySelector("iframe");
+
+const player = new Vimeo(iframe);
+
+player.setCurrentTime(localStorage.getItem(TIME))
+
+player.on("timeupdate", throttle(onPlayerTimeupdate, 1000));
+
+function onPlayerTimeupdate(event) {
+    const currentTime = event.seconds;
+    localStorage.setItem(TIME, currentTime);
+}
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(".feedback-form");
+const STORAGE_FEEDBACK = "feedback-form-storage";
+
+checkFormValue();
+
+form.addEventListener("input", throttle(onFormInput, 500));
+form.addEventListener("submit", onFormSubmit);
+
+function onFormInput(event) {
+  const user = {};
+  user.email = form.querySelector("input").value;
+  user.message = form.querySelector("textarea").value;
+  localStorage.setItem(STORAGE_FEEDBACK, JSON.stringify(user));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === "" || message.value === "") {
+    alert("Заповніть всі поля форми!");
+    return;
+  }
+  const formData = new FormData(event.currentTarget);
+  const user = {};
+  formData.forEach((value, name) => {
+    user[name] = value;
+  });
+
+  console.log(user);
+
+  form.reset();
+  localStorage.removeItem(STORAGE_FEEDBACK);
+}
+
+function checkFormValue() {
+    const checkValid = JSON.parse(localStorage.getItem(STORAGE_FEEDBACK))
+  if (checkValid) {
+    const { email, message } = checkValid;
+    form.querySelector("input").value = email;
+    form.querySelector("textarea").value = message;
+  }
+}
